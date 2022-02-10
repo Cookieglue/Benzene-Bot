641669c76ee5ed4eff5613dfba86c213a03a7e24
mari <63924379+Cookieglue@users.noreply.github.com>
2022-02-10 05:29:49 +0000

    Added commands

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import discord
+from discord.ext import commands
 import os
 
-token = os.environ['TOKEN']
-client = discord.Client()
+TOKEN = os.environ['TOKEN']
+bot = commands.Bot(command_prefix='//')
 
-@client.event
-async def on_ready():
-  print('Logged {0.user}.format(client)')
+@bot.command()
+async def ping(ctx):
+  await ctx.channel.send(f"Pong! {round(bot.latency * 1000)}ms")
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-  if message.content.startswith('*/'):
-    await message.channel.send('Hello!')
-client.run(token)
+@bot.command()
+async def getpfp(ctx):
+  member = ctx.author
+  await ctx.send(member.avatar_url)
+
+bot.run(TOKEN)
